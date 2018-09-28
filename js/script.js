d2228d2d1285a6a69065468cdb0ab6c8d708016d
Truman Smith <trumanwsmith@gmail.com>
2018-09-28 14:03:21 -0700

    add pagination links
    create functions that manage elements containing pagination links.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-console.log("script js is here");
-// Add variables that store DOM elements you will need to reference and/or manipulate
-const pageDiv = document.getElementById('page');
-var students = document.getElementsByClassName('student-item');
 
-//var paginationParent = createPaginationParent(pageDiv);
+// Add variables that store DOM elements you will need to reference and/or manipulate
+const pageDiv = document.querySelector('div.page');
+var students = document.querySelectorAll('.student-item');
+
+var paginationUL = createPaginationParent(pageDiv);
 var pageSize = 10;  // after testing then update to const
-//setPaginationButtons( paginationParent , students.length, pageSize )
+createPagination( paginationUL , students.length, pageSize )
 
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
@@ -18,9 +18,44 @@ var pageSize = 10;  // after testing then update to const
 
 
 
-// Create and append the pagination links - Creating a function that can do this is a good approach
+// Handle pagination links
+// Create and apparent the element that holds the links
+//   Return the UL element that will contain the links
+function createPaginationParent( parentElement ) {
+  let div = document.createElement('div');
+  div.className = 'pagination';
+  let linksUL = document.createElement('ul');
+  div.appendChild(linksUL)
+  parentElement.appendChild(div);
+  return linksUL;
+}
 
+// Add or remove links based on the new calculated page count
+function createPagination( paginationUL, listSize, pageSize ) {
 
+  let newPageCount = Math.ceil( listSize / pageSize );
+  let links = paginationUL.children
+
+  // append needed buttons
+  if ( links.length < newPageCount ) {
+    let newLI;
+    let newLink;
+    for ( let idx = links.length; idx < newPageCount; idx++ ) {
+      newLI = document.createElement('li');
+      newLink = document.createElement('a');
+      newLink.href = '#';
+      newLink.textContent = (idx + 1).toString();
+      newLI.appendChild(newLink);
+      paginationUL.appendChild(newLI);
+    }
+  }
+  // remove unneeded button starting from the last
+  else if ( newPageCount < links.length ) {
+    for ( let idx = (links.length - 1); idx >= newPageCount; idx-- ) {
+      paginationUL.removeChild(links[idx]);
+    }
+  }
+}
 
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
