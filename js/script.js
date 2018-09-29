851ec8f6aa222d281aa88a359b18abf0c8d7411d
Truman Smith <trumanwsmith@gmail.com>
2018-09-29 00:59:27 -0700

    filter students based on page# and search filter

--- js/script.js
+++ js/script.js
@@ -3,38 +3,68 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Add variables that store DOM elements you will need to reference and/or manipulate
-const pageDiv = document.querySelector('div.page');
-var students = document.querySelectorAll('.student-item');
+// Store frequently referenced elements
+const students = document.querySelectorAll('.student-item');
+const paginationUL = createPaginationContainer(document.querySelector('div.page'));
+const pageSize = 10;
+// Create the initial pagination links
+createPaginationLinks( paginationUL , students.length, pageSize );
+// Initally, display page 1 without a search filter
+var searchFilter = '';
+selectPage( students, '1', searchFilter );
 
-var paginationUL = createPaginationParent(pageDiv);
-var pageSize = 10;  // after testing then update to const
-createPagination( paginationUL , students.length, pageSize )
-selectPage( students, '1', '')
+//  Change which students are displayed based on page# and search filter
+//    Returns the number of students that contain the search filter
+function filterList( listItems, pageNum, searchFilter ) {
+  // set the range of matches to display based on page# and page size
+  let firstMatchToDisplay = ((pageNum - 1) * pageSize) + 1;
+  let lastMatchToDisplay = firstMatchToDisplay + pageSize - 1;
 
+  let itemsMatched = 0;
+  let student, name, email;
 
-// Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+  for (let idx = 0; idx < listItems.length; idx++ ) {
+    student = listItems[idx];
+    name = student.querySelector('h3').textContent;
+    email = student.querySelector('.email').textContent;
+    // don't display students that don't contain search filter
+    if ( name.indexOf(searchFilter) === -1 &&
+         email.indexOf(searchFilter) === -1 ) {
+       student.style.display = 'none'
+    } else {  // display students if page has room
+      itemsMatched++;
+       // the page has room to display student
+       if ( itemsMatched >= firstMatchToDisplay &&
+            itemsMatched <= lastMatchToDisplay ) {
+         student.style.display = '';
+      } else { // page is full
+        student.style.display = 'none';
+      }
+    }
+  }
+  return itemsMatched;
+}
 
-// Filter lines based on page and search term, and manage pagination#
-//   'pageNum' should be a string and a value on a pagination link
-function selectPage( listItems, pageNum, filter  ) {
-  setActivePageNumber( paginationUL, pageNum )
-  // filterList( students, pageNum, searchFilter )
+// Filter students based on selected page# and search term
+//   Manage pagination links
+//   Parameters:
+//     pageNum: string. expected to be a value on a pagination link
+//     searchFilter: string. must be in student name or email to display it
+function selectPage( listItems, pageNum, searchFilter  ) {
+  let itemsMatched = filterList( students, pageNum, searchFilter );
+  createPaginationLinks( paginationUL, itemsMatched, pageSize );
+  setActivePageNumber( paginationUL, pageNum );
 }
 
 paginationUL.addEventListener('click', function(event) {
-  selectPage( students, event.target.textContent, '');
+  selectPage( students, event.target.textContent, searchFilter );
 });
-
-
-
 
 // Handle pagination links
 
-// Create and apparent the element that holds the links
+// Create the element that will hold the page# links
 //   Return the UL element that will contain the links
-function createPaginationParent( parentElement ) {
+function createPaginationContainer( parentElement ) {
   let div = document.createElement('div');
   div.className = 'pagination';
   let linksUL = document.createElement('ul');
@@ -44,8 +74,7 @@ function createPaginationParent( parentElement ) {
 }
 
 // Add or remove links based on the new calculated page count
-function createPagination( paginationUL, listSize, pageSize ) {
-
+function createPaginationLinks( paginationUL, listSize, pageSize ) {
   let newPageCount = Math.ceil( listSize / pageSize );
   let links = paginationUL.children
 
@@ -70,29 +99,19 @@ function createPagination( paginationUL, listSize, pageSize ) {
   }
 }
 
-
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
-
-// Highlight and de-highlight page numbers in pagination
-//   De-highlight the currently highlighted page link
-//   Highlight the page link specified by 'pageNum'
+// Highlight and un-highlight page numbers in pagination
+//   Un-highlight the currently highlighted page link
+//   Highlight the page link specified by parameter pageNum
 function setActivePageNumber( paginationUL, pageNum ) {
   let link, newClass
   for ( let pageLI of paginationUL.children ) {
     link = pageLI.querySelector('a');
     newClass = '';
-    // de-highlight currently highlighted page# (maybe)
-    if ( link.className === 'active' ) {
-      newClass = '';
-    }
+    // un-highlight currently highlighted page# (maybe)
+    if ( link.className === 'active' ) newClass = '';
     // highlight the page# as per pageNum parameter
-    if ( link.textContent === pageNum ) {
-      newClass = 'active';
-    }
-    // set the class name if it changed
-    if ( link.className !== newClass ) {
-      link.className = newClass;
-    }
+    if ( link.textContent === pageNum ) newClass = 'active';
+    // update the class name if it changed
+    if ( link.className !== newClass ) link.className = newClass;
   }
 }
