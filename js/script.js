e6644d618878aabc48d7b106f633ceb18dac9fda
Truman Smith <trumanwsmith@gmail.com>
2018-09-29 18:59:01 -0700

    search filter
    add search field and filtering students with it.

--- js/script.js
+++ js/script.js
@@ -3,15 +3,43 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Store frequently referenced elements
+// Store frequently referenced elements and create new elements
 const students = document.querySelectorAll('.student-item');
-const paginationUL = createPaginationContainer(document.querySelector('div.page'));
 const pageSize = 10;
-// Create the initial pagination links
-createPaginationLinks( paginationUL , students.length, pageSize );
-// Initally, display page 1 without a search filter
-var searchFilter = '';
-selectPage( students, '1', searchFilter );
+const searchFilterForm = createSearchElements(
+  document.querySelector('div.page-header'));
+const searchInput = searchFilterForm.querySelector('input');
+const searchButton = searchFilterForm.querySelector('button');
+const paginationUL = createPaginationContainer(
+  document.querySelector('div.page'));
+const msgDisplay = createMessageDisplay(
+  document.querySelector('div.page'));
+
+// Display page 1 with no filter
+displayPage( students, '1', '' );
+
+// Filter students based on selected page# and search term
+//   Manage pagination links
+//   Parameters:
+//     pageNum: string. expected to be a value on a pagination link
+//     searchFilter: string. must be in student name or email to display it
+function displayPage( listItems, pageNum, searchFilter  ) {
+  let itemsMatched = filterList( students, pageNum, searchFilter );
+  createPaginationLinks( paginationUL, itemsMatched, pageSize );
+  setActivePageNumber( paginationUL, pageNum );
+}
+
+paginationUL.addEventListener('click', function(event) {
+  displayPage( students, event.target.textContent, searchInput.value );
+});
+
+searchButton.addEventListener('click', function(event) {
+  displayPage( students, '1', searchInput.value );
+});
+
+searchInput.addEventListener('keyup', function(event) {
+  displayPage( students, '1', searchInput.value );
+});
 
 //  Change which students are displayed based on page# and search filter
 //    Returns the number of students that contain the search filter
@@ -23,15 +51,21 @@ function filterList( listItems, pageNum, searchFilter ) {
   let itemsMatched = 0;
   let student, name, email;
 
-  for (let idx = 0; idx < listItems.length; idx++ ) {
-    student = listItems[idx];
+  // Returns true if student matches search filter
+  function studentMatchesSearch( student, searchFilter ) {
     name = student.querySelector('h3').textContent;
     email = student.querySelector('.email').textContent;
-    // don't display students that don't contain search filter
-    if ( name.indexOf(searchFilter) === -1 &&
-         email.indexOf(searchFilter) === -1 ) {
+    return ( name.indexOf(searchFilter) !== -1 ||
+             email.indexOf(searchFilter) !== -1 );
+  }
+
+  for (let idx = 0; idx < listItems.length; idx++ ) {
+    student = listItems[idx];
+    // don't display students that fail search filter
+    if ( searchFilter.length &&
+         ! studentMatchesSearch( student, searchFilter ) ) {
        student.style.display = 'none'
-    } else {  // display students if page has room
+    } else {  // otherwise display students if page has room
       itemsMatched++;
        // the page has room to display student
        if ( itemsMatched >= firstMatchToDisplay &&
@@ -42,27 +76,51 @@ function filterList( listItems, pageNum, searchFilter ) {
       }
     }
   }
+  displayMessage(msgDisplay, (itemsMatched ? '' : 'No students found'));
   return itemsMatched;
 }
 
-// Filter students based on selected page# and search term
-//   Manage pagination links
-//   Parameters:
-//     pageNum: string. expected to be a value on a pagination link
-//     searchFilter: string. must be in student name or email to display it
-function selectPage( listItems, pageNum, searchFilter  ) {
-  let itemsMatched = filterList( students, pageNum, searchFilter );
-  createPaginationLinks( paginationUL, itemsMatched, pageSize );
-  setActivePageNumber( paginationUL, pageNum );
+// Highlight and un-highlight page numbers in pagination
+//   Un-highlight the currently highlighted page link
+//   Highlight the page link specified by parameter pageNum
+function setActivePageNumber( paginationUL, pageNum ) {
+  let link, newClass
+  for ( let pageLI of paginationUL.children ) {
+    link = pageLI.querySelector('a');
+    newClass = '';
+    // un-highlight currently highlighted page# (maybe)
+    if ( link.className === 'active' ) newClass = '';
+    // highlight the page# as per pageNum parameter
+    if ( link.textContent === pageNum ) newClass = 'active';
+    // update the class name if it changed
+    if ( link.className !== newClass ) link.className = newClass;
+  }
 }
 
-paginationUL.addEventListener('click', function(event) {
-  selectPage( students, event.target.textContent, searchFilter );
-});
+function displayMessage( msgElement, str ) {
+  msgElement.textContent = str;
+}
 
-// Handle pagination links
+// Create the the search input and button
+//   Return the parent to the input and button elements
+function createSearchElements( parentElement ) {
+    let div = document.createElement('div');
+    div.className = 'student-search';
 
-// Create the element that will hold the page# links
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Search for students...';
+    div.appendChild(input);
+
+    let button = document.createElement('button');
+    button.textContent = 'Search';
+    div.appendChild(button);
+
+    parentElement.appendChild(div);
+    return div;
+}
+
+// Create the list that will hold the page# links
 //   Return the UL element that will contain the links
 function createPaginationContainer( parentElement ) {
   let div = document.createElement('div');
@@ -99,19 +157,12 @@ function createPaginationLinks( paginationUL, listSize, pageSize ) {
   }
 }
 
-// Highlight and un-highlight page numbers in pagination
-//   Un-highlight the currently highlighted page link
-//   Highlight the page link specified by parameter pageNum
-function setActivePageNumber( paginationUL, pageNum ) {
-  let link, newClass
-  for ( let pageLI of paginationUL.children ) {
-    link = pageLI.querySelector('a');
-    newClass = '';
-    // un-highlight currently highlighted page# (maybe)
-    if ( link.className === 'active' ) newClass = '';
-    // highlight the page# as per pageNum parameter
-    if ( link.textContent === pageNum ) newClass = 'active';
-    // update the class name if it changed
-    if ( link.className !== newClass ) link.className = newClass;
-  }
+// Create the message field
+//   Return the message display element
+function createMessageDisplay( parentElement ) {
+  let msg = document.createElement('h2');
+  msg.style.color = 'red';
+  msg.style.textAlign = 'center';
+  parentElement.appendChild(msg);
+  return msg
 }
